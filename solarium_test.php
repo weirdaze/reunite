13c6fc2ec1042e3cb20eb0ccbe4c6deadbf7c8c2
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-02 14:49:14 -0400

    test

--- solarium_test.php
+++ solarium_test.php
@@ -1,27 +1,8 @@
 <?php
-echo "hello world";
-require(__DIR__.'init.php');
-htmlHeader();
-
-// check solarium version available
-echo 'Solarium library version: ' . Solarium\Client::VERSION . ' - ';
-
-// create a client instance
-$client = new Solarium\Client($config);
-
-// create a ping query
-$ping = $client->createPing();
-
-// execute the ping query
-try {
-    $result = $client->ping($ping);
-    echo 'Ping query successful';
-    echo '<br/><pre>';
-    var_dump($result->getData());
-    echo '</pre>';
-} catch (Solarium\Exception $e) {
-    echo 'Ping query failed';
-}
-
-htmlFooter();
+$execStr = 'python /var/www/html/scripts/search.py';
+echo $execStr;
+$result = exec($execStr);
+echo "
+";
+echo $result;
 ?>
