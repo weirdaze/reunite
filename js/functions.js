fb068f1eda48ef10fd94853edddf5de36bdf87f7
Jake Zeitz <jguitarz@gmail.com>
2018-07-02 01:29:52 -0400

    resusable function updateContent() added

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+function updateContent(selector,url,data,before,complete){
+	$(selector).html("<div id='spinner'><i class='fa fa-spinner fa-spin fa-7x'></i></div>");
+	before != "" ? before() : "";
+	$.ajax({
+		type: "GET",
+		url: url,
+		data: data,
+		success: function(data){
+			$(selector).html(data);
+			complete != "" ? complete() : "";
+		}
+	});
+}
--- js/main.js
+++ js/main.js
@@ -1,32 +1,22 @@
 $(document).ready(function(){
 	$(document).on("click",".selectPerson",function(){
 		var person_type_id = $(this).data("person_type_id");
-		$.ajax({
-			type: "GET",
-			url: "includes/results.php",
-			data: {"person_type_id": person_type_id},
-			success: function(data){
-				$("#results").html(data);
-				$("#selectPersonType").slideUp(500);
-			}
-		});
+		var complete = function() {
+			$("#selectPersonType").slideUp(500);
+		}
+		updateContent("#results","includes/results.php",{"person_type_id": person_type_id},"",complete);
 	});
 	$(document).on("click",".person",function(){
 		var uid = $(this).data("uid");
 		var person_type_id = $(this).data("person_type_id");
-		$(".modal-body").html("<div id='spinner'><i class='fa fa-spinner fa-spin fa-7x'></i></div>");
-		$(".modal").modal("show");
-		$.ajax({
-			type: "GET",
-			url: "includes/person.php",
-			data: {"uid": uid, "person_type_id": person_type_id},
-			success: function(data){
-				$(".modal-title").text("Juan Doe (UID: "+uid+")");
-				person_type_id == 1 ? $("#claimMember").text("Es mi hijo!") : $("#claimMember").text("Es mi padre!")
-				$(".modal-body").html(data);
-				
-			}
-		});
+		var before = function(){
+			$(".modal").modal("show");
+		}
+		var complete = function(){
+			$(".modal-title").text("Juan Doe (UID: "+uid+")");
+			person_type_id == 1 ? $("#claimMember").text("Es mi hijo!") : $("#claimMember").text("Es mi padre!");
+		}
+		updateContent(".modal-body","includes/person.php",{"uid": uid, "person_type_id": person_type_id},before,complete);
 	});
 	$(document).on("click",".prevArrow, .nextArrow",function(){
 		var uid = $("#personDetails").data("uid");
@@ -34,18 +24,13 @@ $(document).ready(function(){
 		var direction = 1;
 		$(this).hasClass("prevArrow") ? direction = -1 : "";
 		var newUid = uid + direction;
-		$(".modal-body").html("<div id='spinner'><i class='fa fa-spinner fa-spin fa-7x'></i></div>");
-		$(".modal-title").text("");
-		$.ajax({
-			type: "GET",
-			url: "includes/person.php",
-			data: {"uid": newUid, "person_type_id": person_type_id},
-			success: function(data){
-				$(".modal-title").text("Juan Doe (UID: "+newUid+")");
-				person_type_id == 1 ? $("#claimMember").text("Es mi hijo!") : $("#claimMember").text("Es mi padre!")
-				$(".modal-body").html(data);
-				$(".modal").modal("show");
-			}
-		});
+		var before = function(){
+			$(".modal-title").text("");
+		}
+		var complete = function(){
+			$(".modal-title").text("Juan Doe (UID: "+newUid+")");
+			person_type_id == 1 ? $("#claimMember").text("Es mi hijo!") : $("#claimMember").text("Es mi padre!");
+		}
+		updateContent(".modal-body","includes/person.php",{"uid": newUid, "person_type_id": person_type_id},before,complete);
 	});
 });
