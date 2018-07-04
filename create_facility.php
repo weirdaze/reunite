62466ce8d9421f0163bcaf58899e1c274bb2ca1e
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-03 20:17:13 -0400

    test facility 3
    testing again

--- create_facility.php
+++ create_facility.php
@@ -7,7 +7,7 @@
 	$poc = 'test poc';
 
 	$facility = $facility_name.','.$address.','.$city.','.$state.','.$zip.','.$poc;
-
+    echo $facility;
 	$execStr = 'python /var/www/html/reunite/scripts/create_facility.py '.$facility;
 	$result = exec($execStr);
 	echo $result;
