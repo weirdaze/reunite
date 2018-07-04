83819bd2968e431c815198b3e0d8a18bad4867b7
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-04 08:17:17 -0400

    Testing add_user.php
    testing soemthing with Sql query

--- add_user.php
+++ add_user.php
@@ -1,6 +1,6 @@
 <?php
 	include 'header_login.php';
-	// this form is going to be for creating a ticket
+	// this form is going to be for creating a facility
 ?>
 
 <style>
@@ -8,65 +8,41 @@
 		background-color: #eee;
 	}
 </style>
-
-<form class="loginBox signin pb-3" method="post" action="processlogin.php">
-	<div class="bg-info text-light p-2 mb-3 lead">Sign In</div>
+<?php
+    include('config.php');
+    $sql = "SELECT FacilityNumber, FacilityName FROM facilities";
+	$result = mysqli_query($db,$sql);
+	while($row = $result->fetch_assoc()) {
+	        echo $row['FacilityNumber'] . " " . $row['FacilityName'];
+	}
+?>
+<form class="loginBox signin pb-3" method="post" action="create_facility.php">
+	<div class="bg-info text-light p-2 mb-3 lead">Register</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-user"></i></div>
-		<input class="form-control mb-2" type="text" name="username" placeholder="Username">
+		<input class="form-control mb-2" type="text" name="facility_name" placeholder="Name of the Facility">
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-lock"></i></div>
-		<input class="form-control mb-2" type="password" name="password" placeholder="Password">
+		<input class="form-control mb-2" type="text" name="address" placeholder="address">
 	</div>
-	<input class="btn btn-primary mx-3" type="submit" value="Sign In">
+	<div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-lock"></i></div>
+		<input class="form-control mb-2" type="text" name="city" placeholder="city">
+	</div><div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-lock"></i></div>
+		<input class="form-control mb-2" type="text" name="state" placeholder="state">
+	</div>
+	<div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-lock"></i></div>
+		<input class="form-control mb-2" type="text" name="zip" placeholder="zip">
+	</div>
+	<div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-lock"></i></div>
+		<input class="form-control mb-2" type="text" name="poc" placeholder="point of contact full name">
+	</div>
+	<input class="btn btn-primary mx-3" type="submit" value="Create">
 	<hr class="mx-3" />
-	<span class="small mx-3">Don't have an account? <a class="switchLogin text-primary">Sign Up Here</a></span>
 </form>
-<form class="loginBox signup pb-3" method="post" action="processlogin.php">
-	<div class="bg-info text-light p-2 mb-3 lead">Sign Up</div>
-	<div class="form-group mx-3">
-		<div class="input-icon"><i class="fa fa-id-card"></i></div>
-		<input class="form-control mb-2" type="text" name="firstname" placeholder="First Name">
-	</div>
-	<div class="form-group mx-3">
-		<div class="input-icon"><i class="fa fa-id-card"></i></div>
-		<input class="form-control mb-2" type="text" name="middlename" placeholder="Middle Name">
-	</div>
-	<div class="form-group mx-3">
-		<div class="input-icon"><i class="fa fa-id-card"></i></div>
-		<input class="form-control mb-2" type="text" name="lastname" placeholder="Last Name">
-	</div>
-	<div class="form-group mx-3">
-		<div class="input-icon"><i class="fa fa-id-card"></i></div>
-		<input class="form-control mb-2" type="text" name="employeeid" placeholder="Employee ID">
-	</div>
-	<div class="form-group mx-3">
-		<div class="input-icon"><i class="fa fa-user"></i></div>
-		<input class="form-control mb-2" type="text" name="createusername" placeholder="Create Username">
-	</div>
-	<div class="form-group mx-3">
-		<div class="input-icon"><i class="fa fa-lock"></i></div>
-		<input class="form-control mb-2" type="password" name="createpassword" placeholder="Password">
-	</div>
-	<div class="form-group mx-3">
-		<div class="input-icon"><i class="fa fa-lock"></i></div>
-		<input class="form-control mb-2" type="password" name="confirmpassword" placeholder="Confirm Password">
-	</div>
-	<input class="btn btn-primary mx-3" type="submit" value="Sign Up">
-	<hr class="mx-3" />
-	<span class="small mx-3">Already have an account? <a class="switchLogin text-primary">Sign In Here</a></span>
-</form>
-
-<script>
-	setTimeout(function(){
-		$(".alert-success").slideUp(300);
-	},2500);
-	
-	$(".switchLogin").click(function(){
-		$(".signin, .signup").toggle();
-		$(".alert").slideUp(300);
-	});
-</script>
 
 <?php include 'footer_login.php'?>
