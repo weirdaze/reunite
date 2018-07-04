e8c4276534643264fa01246c3a683e56e7cbc781
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-03 20:11:48 -0400

    testing facility entry
    verified python script works. testing php script now without post params

--- create_facility.php
+++ create_facility.php
@@ -1,15 +1,22 @@
 <?php
-	$facility_name = $_POST['facility_name'];
+	/*$facility_name = $_POST['facility_name'];
 	$address = $_POST['address'];
 	$city = $_POST['city'];
 	$state = $_POST['state'];
 	$zip = $_POST['zip'];
-	$poc = $_POST['poc'];
+	$poc = $_POST['poc'];*/
+    
+    $facility_name = 'test facility';
+	$address = 'test address';
+	$city = 'test city';
+	$state = 'test state';
+	$zip = 'test zip';
+	$poc = 'test poc';
 
 	$facility = $facility_name.','.$address.','.$city.','.$state.','.$zip.','.$poc;
 
 	$execStr = 'python /var/www/html/reunite/scripts/create_facility.py '.$facility;
 	$result = exec($execStr);
 	echo $result;
-	header("Location: index.php");
+	//header("Location: index.php");
 ?>
--- scripts/create_facility.py
+++ scripts/create_facility.py
@@ -4,3 +4,4 @@ import facilities
 facility = sys.argv[1]
 
 facilities.create_facility(facility)
+print "done"
