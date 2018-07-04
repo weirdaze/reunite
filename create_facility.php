59e44b797d5cc75b9393be549996e981b73a58d1
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-03 20:20:35 -0400

    test5 facility
    test

--- create_facility.php
+++ create_facility.php
@@ -6,7 +6,7 @@
 	$zip = 'test zip';
 	$poc = 'test poc';
 
-	$facility = "\'".$facility_name.','.$address.','.$city.','.$state.','.$zip.','.$poc."\'";
+	$facility = "'".$facility_name.','.$address.','.$city.','.$state.','.$zip.','.$poc."'";
     echo $facility;
 	$execStr = 'python /var/www/html/reunite/scripts/create_facility.py '.$facility;
 	$result = exec($execStr);
