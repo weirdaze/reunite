45a42a27b668d2d843f747e52f880bfade2bf113
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-05 13:36:22 -0400

    claim button
    these may be all we need to get a match... will see

---
+++ its_me.php
@@ -0,0 +1,32 @@
+<?php
+	$uid_b = $_POST["uid_b"];
+	$first_name = $_POST["first_name"];
+	$middle_name = $_POST["middle_name"];
+	$last_name = $_POST["last_name"];
+	$maternal_last_name = $_POST["maternal_last_name"];
+	$sex = $_POST["sex"];
+	$dob = $_POST["dob"];
+
+	include('config.php');
+
+	$sql = "SELECT * FROM person where 
+	        UPPER(FirstName) like UPPER('%$first_name%') or UPPER(LastName) like UPPER('%$last_name%') 
+	        UPPER(MiddleName) like UPPER('%$middle_name%') or UPPER(MaternalLastName) like UPPER('%$maternal_last_name%')
+	        UPPER(DOB) like UPPER('%$dob%') or UPPER(Sex) like UPPER('%$sex%')
+	        order by LastName";
+	echo $sql;
+
+	$result = mysqli_query($db,$sql);
+
+	if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+	?>
+			<div class="person d-flex align-items-center flex-column justify-content-center" data-uid="<?php echo $row['UID']; ?>" data-gender="<?php echo $row['Sex']; ?>" data-fullname="<?php echo $row['FirstName'] . ' ' . $row['LastName']; ?>">
+				<img src="media/photo/<?php echo $row['photo']; ?>" />
+				<div class="caption"><?php echo $row['LastName'] . ", " . $row['FirstName']; ?></div>
+			</div>	
+	<?php
+			}
+		}
+	?>
+?>
---
+++ who_are_you.php
@@ -0,0 +1,44 @@
+<?php
+	include 'header_login.php';
+	$uid_b = $_POST["uid"];
+
+?>
+
+<style>
+	body {
+		background-color: #eee;
+	}
+</style>
+<form class="loginBox signin pb-3" method="post" action="create_facility.php">
+	<input type="hidden" name="uid_b" value="<?php echo $uid_b ?>" />
+	<div class="bg-info text-light p-2 mb-3 lead">Who Are you?</div>
+	<div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-user"></i></div>
+		<input class="form-control mb-2" type="text" name="first_name" placeholder="First Name">
+	</div>
+    <div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-user"></i></div>
+		<input class="form-control mb-2" type="text" name="middle_name" placeholder="Middle Name">
+	</div>
+	<div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-user"></i></div>
+		<input class="form-control mb-2" type="text" name="last_name" placeholder="Last Name">
+	</div>
+	<div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-user"></i></div>
+		<input class="form-control mb-2" type="text" name="maternal_last_name" placeholder="Second Last Name">
+	</div>
+    <div class="form-group mx-3">
+		<input class="form-control mb-2" type="date" name="dob" placeholder="Date of Birth">
+	</div>
+	<div class="form-group mx-3">
+		<select class="form-control mb-2" name="sex" required>
+			<option value="M">M</option>
+			<option value="F">F</option>
+		</select>
+	</div>
+	<input class="btn btn-primary mx-3" type="submit" value="Create">
+	<hr class="mx-3" />
+
+</form>
+<?php include 'footer_login.php'?>
