9d70c76b4eec04a7fe1c01099fe80af7430fadd3
Jake Zeitz <jguitarz@gmail.com>
2018-07-06 18:08:46 -0400

    add_user updates

--- processclaim.php
+++ processclaim.php
@@ -1,9 +1,19 @@
 <?php
 	session_start();
 	if(isset($_GET['uid'])){
-		$_SESSION['uid'] = $_GET['uid'];
-	}
+		if($_GET['claim_type'] == "child"){
+			$_SESSION['uid'] = $_GET['uid'];
+		}
+		else {
+			$uid_a = $_GET['uid'];
+			$uid_b = $_SESSION['uid'];
 
-	/*echo $_SESSION['uid'];
-	header("Location: index.php");*/
+			$uids = "'" . $uid_a . ',' . $uid_b . "'";
+			$execStr = 'python /var/www/html/reunite/scripts/submit_claim.py ' . $uids;
+			echo $execStr;
+			/*$result = exec($execStr);*/
+			/*echo $result;*/
+			/*echo "Entered Claim: " . $result;*/
+		}	
+	}
 ?>
