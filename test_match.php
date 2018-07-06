affb83a0097159e68962b9c6792fab23d05cf880
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-06 12:55:46 -0400

    test match again
    test match part 2

--- test_match.php
+++ test_match.php
@@ -11,11 +11,11 @@
 	<div class="bg-info text-light p-2 mb-3 lead">Who Are you?</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-user"></i></div>
-		<input class="form-control mb-2" type="text" name="uid" placeholder="UID">
+		<input class="form-control mb-2" type="text" name="uid_a" placeholder="UID">
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-user"></i></div>
-		<input class="form-control mb-2" type="text" name="uid" placeholder="UID">
+		<input class="form-control mb-2" type="text" name="uid_b" placeholder="UID">
 	</div>
 	<input class="btn btn-primary mx-3" type="submit" value="Create">
 	<hr class="mx-3" />
