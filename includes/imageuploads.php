ca1c37e3f0b8ab161a5b1e8f06610b5843935fb2
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-09 15:43:14 -0400

    removing temp dir for images and videos

--- includes/imageuploads.php
+++ includes/imageuploads.php
@@ -1,13 +1,11 @@
 <?php
 	session_start();
-	$target_dir = "media/photo/temp/";
+	$target_dir = "media/photo/";
 	$ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
 	$current_iuser_id = $_POST["iuid"];
 	$execStr = "python /var/www/html/reunite/scripts/rename_upload.py ".$_FILES['image']['name']." ".$current_iuser_id."  photo";
 	//$target_file = $target_dir . basename( $_FILES['image']['name'],'.' . $ext) . "_" . $_SESSION['userid'] . '.' . $ext;
-	//echo $execStr;
 	$result1 = exec($execStr);
-	//echo $result1;
 	$target_file = $target_dir . $result1;
 	$target_file_dir = "/var/www/html/reunite/" . $target_file;
 	$uploadOk = 1;
