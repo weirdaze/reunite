c4420da444bb7d03d671a076179bd9fd60f6bc5e
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-09 15:55:47 -0400

    stab in the dark on image name

--- add_user.php
+++ add_user.php
@@ -151,12 +151,14 @@
 		    	data: img,
 		    	success: function(data){
 		    		$("#imgThumbnails").find("i").remove();
-		    		$("#imgThumbnails").append("data");
+		    		$("#imgThumbnails").append(data);
 		    		var photoLabel = $("#photo").next(".custom-file-label");
 		    		var newText = photoLabel.text();
 		    		newText == "Upload Photos" ? newText = filename : newText = newText + "," + filename;
 		    		photoLabel.removeClass("placeholder").text(newText);
 		    		$("#photos").val(newText);
+		    		var imageName = $("#imageName").val();
+		    		$("#photo").val(imageName);
 		    	}
 		    });
 		});
