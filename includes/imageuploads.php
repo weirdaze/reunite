c30e367f3d2d6d9c0ddcf95d673224b097828df3
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-09 12:47:01 -0400

    image uploads 1

--- includes/imageuploads.php
+++ includes/imageuploads.php
@@ -5,7 +5,9 @@
 	$current_iuser_id = $_POST["iuid"];
 	$execStr = "python /var/www/html/reunite/scripts/rename_upload.py ".$_FILES['image']['name']." ".$current_iuser_id."  photo";
 	//$target_file = $target_dir . basename( $_FILES['image']['name'],'.' . $ext) . "_" . $_SESSION['userid'] . '.' . $ext;
+	echo $execStr;
 	$result1 = exec($execStr);
+	echo $result1;
 	$target_file = $target_dir . $result1;
 	$target_file_dir = "/var/www/html/reunite/" . $target_file;
 	$uploadOk = 1;
