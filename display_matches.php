a717a412c7eb8c6ddcdb2babb813cd6aefb1adbc
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-10 18:01:42 -0400

    changing display matches to sort by date matched and to include names
    make it more demo-able

--- display_matches.php
+++ display_matches.php
@@ -15,7 +15,7 @@
 		</tr>
 <?php
 		
-	$sql = "SELECT Match_ID, UID_A, UID_B, Status from matches where Status<>'closed'";
+	$sql = "SELECT Match_ID, UID_A, UID_B, DateMatched, Status from matches where Status<>'closed' ORDER BY DateMatched";
 
 	$result = mysqli_query($db,$sql);
 
@@ -23,13 +23,28 @@
 		while($row = $result->fetch_assoc()) {
 			$uid_a = $row['UID_A'];
 			$uid_b = $row['UID_B'];
+			$date_matched = $row['DateMatched'];
+			
+			$sql2 = "SELECT FirstName, LastName from person where UID='$uid_a'";
+			$result2 = mysqli_query($db,$sql2);
+			$row2 = $result2->fetch_assoc();
+			$uid_a_fn = $row2['FirstName'];
+			$uid_a_ln = $row2['LastName'];
+
+			$sql3 = "SELECT FirstName, LastName from person where UID='$uid_b'";
+			$result3 = mysqli_query($db,$sql3);
+			$row3 = $result3->fetch_assoc();
+			$uid_b_fn = $row3['FirstName'];
+			$uid_b_ln = $row3['LastName'];
+
 ?>
 			<tr>
 				<td><a class="previewMatch text-primary ml-3" data-matchID="<?php echo $row['Match_ID']; ?>" data-uid_a="<?php echo $row['UID_A']; ?>" data-uid_b="<?php echo $row['UID_B']; ?>"><i class="fa fa-eye"></i></a></td>
 				<td><?php echo $row['Match_ID']; ?></td>
-				<td><?php echo $row['UID_A']; ?></td>
-				<td><?php echo $row['UID_B']; ?></td>
+				<td><?php echo $uid_a_ln.", ".$uid_a_fn." (".$row['UID_A'].")"; ?></td>
+			    <td><?php echo $uid_b_ln.", ".$uid_b_fn." (".$row['UID_B'].")"; ?></td>
 				<td><?php echo $row['Status']; ?></td>
+				<td><?php echo $row['DateMatched']; ?></td>
 			</tr>
 <?php
 		}
