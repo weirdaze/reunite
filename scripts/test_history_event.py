b5f91e1ff1525fad4a8eef6efd0a157ebfcd5788
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-11 09:00:12 -0400

    testing assign ticket

--- scripts/test_history_event.py
+++ scripts/test_history_event.py
@@ -3,8 +3,10 @@ import sys
 
 ticket_number = sys.argv[1]
 userid = sys.argv[2]
-update = sys.argv[3]
+#update = sys.argv[3]
 
 # tickets.insert_history_event(ticket_number + "," + update + "," + userid)
 
-tickets.change_status(ticket_number, update, userid)
+# tickets.change_status(ticket_number, update, userid)
+
+tickets.assign_ticket(ticket_number, userid, userid)
