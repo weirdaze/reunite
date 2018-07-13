803a979fc5beb9e39ed0770fdbc4f9b4a6177725
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-13 14:21:53 -0400

    facilities

--- facility.php
+++ facility.php
@@ -72,11 +72,9 @@
         	document.getElementById("city1").innerHTML = "input cannot contain apostrophes";
         	return false;
     	} 
-    	if (zip != ""){
-    		if (isNaN(zip) || zip < 1 || zip > 99999) {
-        		document.getElementById("zip1").innerHTML = "invalid zip code";
-        		return false;
-    		}
+    	if (zip.includes("'")){
+        	document.getElementById("zip1").innerHTML = "invalid zip code";
+        	return false;
     	} 
     	if (poc.includes("'")) {
         	document.getElementById("poc1").innerHTML = "input cannot contain apostrophes";
