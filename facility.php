36a22ee23a322a066ad66cb97c612eed9542d000
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-13 17:34:21 -0400

    validation

--- facility.php
+++ facility.php
@@ -49,7 +49,9 @@
 		<input class="form-control mb-2" type="text" name="poc" placeholder="Notes" pattern="[0-9A-Za-z :/?&)(.#-]+">
 		<small><p class="bg-warning" id="poc1"></p></small>
 	</div>
-	<small><p class="bg-warning" id="message1">commas or apostrophes are not allowed as inputs</p></small>
+	<div class="form-group mx-3">
+		<small><p class="bg-warning" id="message1">commas or apostrophes are not allowed as inputs</p></small>
+	</div>
 	<input class="btn btn-primary ml-3 mr-2" type="submit" value="Create">
 	<a href="admintools.php" class="btn btn-secondary">Cancel</a>
 </form>
