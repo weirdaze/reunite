c267676046584e4b05de387347206d7ccf092c96
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-13 17:23:09 -0400

    poc validation

--- facility.php
+++ facility.php
@@ -18,7 +18,7 @@
 
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-building"></i></div>
-		<input class="form-control mb-2" type="text" name="facility_name" placeholder="Name of the facility" pattern="[A-Za-z0-9 -]+" required>
+		<input class="form-control mb-2" type="text" name="facility_name" placeholder="Name of the facility" pattern="[A-Za-z0-9 &#.-]+" required>
 		<small><p class="bg-warning" id="facility_name1"></p></small>
 	</div>
 	<div class="form-group mx-3">
@@ -46,7 +46,7 @@
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-user"></i></div>
-		<input class="form-control mb-2" type="text" name="poc" placeholder="Point of contact full name">
+		<input class="form-control mb-2" type="text" name="poc" placeholder="Point of contact full name" patter="[0-9A-Za-z :/?&.#-]+">
 		<small><p class="bg-warning" id="poc1"></p></small>
 	</div>
 	<input class="btn btn-primary ml-3 mr-2" type="submit" value="Create">
