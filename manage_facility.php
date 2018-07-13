711206ae9e527829b3ffeda79f9124ecf568ad53
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-13 17:47:14 -0400

    add user validation

--- manage_facility.php
+++ manage_facility.php
@@ -30,17 +30,17 @@
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-building"></i></div>
-		<input class="form-control mb-2" type="text" name="facility_name" value="<?php echo $facility_name; ?>" required>
+		<input class="form-control mb-2" type="text" name="facility_name" value="<?php echo $facility_name; ?>" pattern="[A-Za-z0-9 &#.-]+" required>
 		<small><p class="bg-warning" id="facility_name1"></p></small>
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-address-card"></i></div>
-		<input class="form-control mb-2" type="text" name="address" placeholder="Address" value="<?php echo $address; ?>">
+		<input class="form-control mb-2" type="text" name="address" placeholder="Address" value="<?php echo $address; ?>" pattern="[0-9]+[ -][0-9A-Za-z .#-]+">
 		<small><p class="bg-warning" id="address1"></p></small>
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-address-card"></i></div>
-		<input class="form-control mb-2" type="text" name="city" placeholder="City" value="<?php echo $city; ?>"required>
+		<input class="form-control mb-2" type="text" name="city" placeholder="City" value="<?php echo $city; ?>" pattern="[A-Za-z0-9 &#.-]+" required>
 		<small><p class="bg-warning" id="city1"></p></small>
 	</div>
 	<div class="form-group mx-3">
@@ -54,13 +54,16 @@
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-address-card"></i></div>
-		<input class="form-control mb-2" type="text" name="zip" placeholder="Zip" maxlength="5" value="<?php echo $zip; ?>">
+		<input class="form-control mb-2" type="text" name="zip" placeholder="Zip" maxlength="5" value="<?php echo $zip; ?>" pattern="^[0-9]{5}(?:-[0-9]{4})?$">
 		<small><p class="bg-warning" id="zip1"></p></small>
 	</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-user"></i></div>
-		<input class="form-control mb-2" type="text" name="poc" placeholder="Point of contact full name" value="<?php echo $poc; ?>">
+		<input class="form-control mb-2" type="text" name="poc" placeholder="Notes" value="<?php echo $poc; ?>" pattern="[0-9A-Za-z :/?&)(.#-]+">
 		<small><p class="bg-warning" id="poc1"></p></small>
+	</div>
+	<div class="form-group mx-3">
+		<small><p class="bg-warning" id="message1">commas or apostrophes are not allowed as inputs</p></small>
 	</div>
 	<input class="btn btn-primary ml-3 mr-2" type="submit" value="Update">
 	<a href="admintools.php" class="btn btn-secondary">Cancel</a>
