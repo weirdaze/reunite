9bde9efda2fdfb437e79f2c80e71293954558011
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-15 22:31:49 -0400

    forgot the semicolon!!! arg!!

--- change_ticket_status.php
+++ change_ticket_status.php
@@ -2,7 +2,7 @@
 	session_start();
 	$ticket_number = $_POST['ticket_number'];
 	$userid = $_SESSION['userid'];
-	$status = $_POST['status']
+	$status = $_POST['status'];
 	// echo "this is the user ID:".$userid;
 	$execStr = "python /var/www/html/reunite/scripts/change_ticket_status.py ".$ticket_number." ".$userid." ".$status;
 	$result = exec($execStr);
