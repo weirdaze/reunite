5ebffcee5d81c02f7be447d265eb94ff9580760f
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-15 22:56:47 -0400

    ok. all fixed!

--- update_ticket_status.php
+++ update_ticket_status.php
@@ -6,6 +6,6 @@
 	// echo "this is the user ID:".$userid;
 	$execStr = "python /var/www/html/reunite/scripts/update_ticket.py ".$ticket_number." ".$userid." '".$update."'";
 	$result = exec($execStr);
-	echo $execStr;
+	//echo $execStr;
 	//header("Location: display_tickets.php");
 ?>
