a4b2850a66a628298595b02ed4d9f8eb2b534034
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-15 22:53:13 -0400

    needed to update quote

--- update_ticket_status.php
+++ update_ticket_status.php
@@ -4,7 +4,7 @@
 	$userid = $_SESSION['userid'];
 	$update = $_POST['update'];
 	// echo "this is the user ID:".$userid;
-	$execStr = "python /var/www/html/reunite/scripts/update_ticket.py ".$ticket_number." ".$userid." ".$update;
+	$execStr = "python /var/www/html/reunite/scripts/update_ticket.py ".$ticket_number." ".$userid." '".$update."'";
 	$result = exec($execStr);
 	echo $execStr;
 	//header("Location: display_tickets.php");
