988f36a8c7b16819326bb595eb15fa7a57fdb60d
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-15 22:09:19 -0400

    testing self assign ticket

--- admintools.php
+++ admintools.php
@@ -20,7 +20,7 @@
 				</a>
 				<a class="list-group-item d-flex align-items-center" href="facility.php">
 					<span class="menuIcon"><i class="fa fa-building"></i></span>
-					<span class="menuText">Create Facility</span>
+					<span class="menuText">Manage Facilities</span>
 				</a>
 				<a class="list-group-item d-flex align-items-center" href="display_tickets.php">
 					<span class="menuIcon"><i class="fa fa-file-alt"></i></span>
---
+++ test_assign_me.php
@@ -0,0 +1,20 @@
+<?php
+	include 'header_login.php';
+?>
+
+<style>
+	body {
+		background-color: #eee;
+	}
+</style>
+<form class="loginBox signin pb-3" method="post" action="change_ticket_agent.php">
+	<div class="bg-info text-light p-2 mb-3 lead">Who Are you?</div>
+	<div class="form-group mx-3">
+		<div class="input-icon"><i class="fa fa-user"></i></div>
+		<input class="form-control mb-2" type="text" name="ticket_number" placeholder="Ticket Number">
+	</div>
+	<input class="btn btn-primary mx-3" type="submit" value="Change">
+	<hr class="mx-3" />
+
+</form>
+<?php include 'footer_login.php'?>
