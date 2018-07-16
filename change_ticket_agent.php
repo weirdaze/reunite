c21f16a3c58ffc70c05c386e1f1992f726de209f
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-15 22:11:58 -0400

    change ticket test

--- change_ticket_agent.php
+++ change_ticket_agent.php
@@ -4,6 +4,7 @@
 	// echo "this is the user ID:".$userid;
 	$execStr = "python /var/www/html/reunite/scripts/change_ticket_agent.py ".$ticket_number." ".$userid;
 	$result = exec($execStr);
-
+	echo $execStr;
+	
 	//header("Location: display_tickets.php");
 ?>
