e0b7e3a2059976908d4801e5a6d9ed825adb7004
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-19 23:06:55 -0400

    default search string

--- test_searching.php
+++ test_searching.php
@@ -13,7 +13,7 @@
 	<div class="bg-info text-light p-2 mb-3 lead">Search</div>
 	<div class="form-group mx-3">
 		<div class="input-icon"><i class="fa fa-user"></i></div>
-		<input class="form-control mb-2" type="text" name="search_string" placeholder="enter your search query">
+		<input class="form-control mb-2" type="text" name="search_string" value='*' placeholder="enter your search query">
 	</div>
 	<input class="btn btn-primary mx-3" type="submit" value="Search">
 	<hr class="mx-3" />
