f2819486b081025281858e4cbfe72a5cd70421b9
Elliott Castillo <elliott.castillo@gmail.com>
2018-07-20 10:42:24 -0400

    testing update search

--- test_search_child.php
+++ test_search_child.php
@@ -5,12 +5,15 @@
 
 	use Elasticsearch\ClientBuilder;
 
+	$from = 0;
+	$sizes = 12;
+
 	$client = ClientBuilder::create()->build();
 	$params = array();
 	$params['index'] = 'person';
 	$params['type'] = 'person';
-	$params['from'] = 0;
-	$params['size'] = 12;
+	$params['from'] = $from;
+	$params['size'] = $sizes;
 	//$params['sort']['firstname']['order'] = 'asc';
 	$params['body']['query']['query_string']['default_field'] = "*";
 	$params['body']['query']['query_string']['query'] = "(".$search_string.") AND (child)";
@@ -20,7 +23,20 @@
 	$hits = count($response['hits']['hits']);
 	$result = null;
 	$i = 0;
-	echo "this is the number of hits: ".$hits;
+	
+	$count_params = array();
+	$count_params['index'] = 'person';
+	$count_params['type'] = 'person';
+	$count_params['body']['query']['query_string']['default_field'] = "*";
+	$count_params['body']['query']['query_string']['query'] = "(".$search_string.") AND (adult)";
+	$counter = $client->count($count_params);
+
+	$final_count = $counter['count'];
+	$pages = ceil($final_count/$sizes);
+	echo "this is the number of total hits: ".$final_count."<br>";
+	echo "this is how many pages we would get: ".$pages."<br>";
+
+	
 	while ($i < $hits) {
 		$result[$i] = $response['hits']['hits'][$i]['_source'];
 		$i++;
