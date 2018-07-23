79aa0ae4bd809ae3283d1e2a6accaa4ed5aca8c9
Jake Zeitz <jguitarz@gmail.com>
2018-07-23 17:23:55 -0400

    iframe 100% w and h

--- test_graph.php
+++ test_graph.php
@@ -1,7 +1,6 @@
 <?php
 	$admin = 1;
 	include 'header.php';
-	session_start();
 	// this form is going to be for creating a user
 ?>
 
@@ -10,7 +9,7 @@
 		background-color: #eee;
 	}
 </style>
-<iframe src="https://tun1.centerpointcc.net:5601/app/kibana#/dashboard/f63b2200-8e92-11e8-8aef-db8a9a9661f9?embed=true&_g=(refreshInterval%3A(display%3AOff%2Cpause%3A!f%2Cvalue%3A0)%2Ctime%3A(from%3Anow%2FM%2Cmode%3Aquick%2Cto%3Anow%2FM))" height="800" width="1000"></iframe>
+<iframe src="https://tun1.centerpointcc.net:5601/app/kibana#/dashboard/f63b2200-8e92-11e8-8aef-db8a9a9661f9?embed=true&_g=(refreshInterval%3A(display%3AOff%2Cpause%3A!f%2Cvalue%3A0)%2Ctime%3A(from%3Anow%2FM%2Cmode%3Aquick%2Cto%3Anow%2FM))" height="100%" width="100%"></iframe>
 
 
 <?php include 'footer_login.php'?>
